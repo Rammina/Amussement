c8b9ad19fe685e3a4fb33af70b92793d8d4166f3
Rammina <altinaschwarzer@gmail.com>
2021-02-22 00:51:11 -0700

    Designed ScrollToBottom using emotion/css

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,13 +1,24 @@
+import DownArrowImg from "../../icons/down-arrow-2.png";
+
 import "./Messages.scss";
 
 import React, { useContext } from "react";
 import { connect } from "react-redux";
 
 import ScrollToBottom from "react-scroll-to-bottom";
+import { css } from "@emotion/css";
 
 import Message from "./Message/Message";
 
 import { ChatContext } from "../AppContext";
+
+// const scrollToBottomCss = css({
+//   backgroundColor: #07050f,
+//   width: 2.25rem,
+//   height: 2.25rem,
+//   bottom: 1rem,
+//   border-radius: 50%
+// });
 
 const Messages = ({ messages, name }) => {
   const { loadMoreMessages, noMoreMessagesToLoad } = useContext(ChatContext);
@@ -28,7 +39,24 @@ const Messages = ({ messages, name }) => {
 
   if (!messages || messages.length < 1) return null;
   return (
-    <ScrollToBottom className="messages messages-container">
+    <ScrollToBottom
+      className="messages messages-container"
+      followButtonClassName={css({
+        backgroundColor: "#07050f",
+        width: "2.25rem",
+        height: "2.25rem",
+        bottom: "1rem",
+        right: "2rem",
+        borderRadius: "50%",
+        backgroundImage: `url(${DownArrowImg})`,
+        backgroundSize: "1.5rem",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        "&:hover": {
+          backgroundColor: "#14121c",
+        },
+      })}
+    >
       {renderLoadMoreMessagesButton()}
       {messages.map((message, i) => {
         console.log(message);
@@ -66,3 +94,44 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, {})(Messages);
+
+/* For implimenting listening to scroll at the top of a page
+https://codepen.io/teimurjan/pen/NzMgKz?#
+
+class App extends React.Component {
+  componentDidMount() {
+    window.onscroll = function() {
+      if(window.pageYOffset === 0) {
+        alert('I AM AT THE TOP');
+      }
+    };
+  }
+  
+  componentWillUnmount() {
+    window.onscroll = null;
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+*/
