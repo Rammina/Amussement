26d6ffd47922ccafb20e197591222e2fd68309e0
Rammina <AltinaSchwarzer@gmail.com>
2021-04-24 09:53:43 +0800

    fix the bug with & characters on query string when redirecting

--- client/src/components/AuthenticatedRoute.js
+++ client/src/components/AuthenticatedRoute.js
@@ -2,18 +2,24 @@ import React, { useContext } from "react";
 import { Route, Redirect, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 
+// note: this is only a temporary solution to prevent & characters from cutting out the redirect link
+const getProcessedQueryString = (queryString) => {
+  if (!queryString) return "";
+  return queryString.replace(/&/g, "AMPERSAND_PLACEHOLDER");
+};
+
 function AuthenticatedRoute(props) {
   const { children, ...rest } = props;
   const { pathname, search } = useLocation();
-  console.log(search);
-  // note: it wont pick up the entire search URL if there are & characters
-  // so it just cuts off especially for chat component :3
+
+  const processedSearch = getProcessedQueryString(search);
+
   return (
     <Route {...rest}>
       {props.isAuthenticated ? (
         children
       ) : (
-        <Redirect to={`/auth/login?redirect=${pathname}${search}`} />
+        <Redirect to={`/auth/login?redirect=${pathname}${processedSearch}`} />
       )}
     </Route>
   );
--- client/src/components/UnauthenticatedRoute.js
+++ client/src/components/UnauthenticatedRoute.js
@@ -3,18 +3,32 @@ import { Route, Redirect, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 import queryString from "query-string";
 
+// check the authenticated route component, this is to avoid having the & characters cut out
+const getOriginalQueryString = (processedString) => {
+  if (!processedString) return "";
+  return processedString.replace(/AMPERSAND_PLACEHOLDER/g, "&");
+};
+
 function UnauthenticatedRoute(props) {
   const { children, ...rest } = props;
   const { search } = useLocation();
 
   const { redirect } = queryString.parse(search);
-  console.log(redirect);
+  // need to process redirect
+  const originalRedirect = getOriginalQueryString(redirect);
+
   return (
     <Route {...rest}>
       {!props.isAuthenticated ? (
         children
       ) : (
-        <Redirect to={redirect === "" || redirect === null ? "/" : redirect} />
+        <Redirect
+          to={
+            originalRedirect === "" || originalRedirect === null
+              ? "/"
+              : originalRedirect
+          }
+        />
       )}
     </Route>
   );
